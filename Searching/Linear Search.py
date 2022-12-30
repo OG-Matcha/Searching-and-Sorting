c1f7bdec39c1b47355dfe97911af3aba3d0a906d
OG-Matcha <matchaskill@gmail.com>
2022-12-30 11:50:48 +0800

    add linear search

---
+++ Searching/Linear Search.py
@@ -0,0 +1,23 @@
+# Linear Search
+
+
+def linear_search(arr, target):
+    for i, v in enumerate(arr):
+        if v == target:
+            return i
+    else:
+        return -1
+
+
+# Time complexity = O(n)
+# Space complexity = O(1)
+
+arr = [1, 4, 6, 9, 12, 15, 16, 25]
+target = 16
+
+result = linear_search(arr, target)
+
+if result == -1:
+    print("Element not found")
+else:
+    print(f"Element found at index {result}")
