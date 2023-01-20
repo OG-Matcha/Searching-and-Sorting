c43851c2b168739c03fdd01faa8f7ceb396aeb6e
OG_Matcha <matchaskill@gmail.com>
2023-01-20 22:21:33 +0800

    Update Binary Search.py

--- Searching/Binary Search.py
+++ Searching/Binary Search.py
@@ -1,3 +1,9 @@
+"""
+Binary Search: is a search algorithm that finds the position of a target value within a sorted array. 
+It compares the target value to the middle element of the array; 
+if they are unequal, the half in which the target cannot lie is eliminated and the search continues on the remaining half until it is successful.
+"""
+
 # Recursive Binary Search
 
 
