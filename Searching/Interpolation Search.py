2fc291d16719b05925ad820624aaa43e0605f439
OG-Matcha <matchaskill@gmail.com>
2023-01-21 15:36:23 +0800

    add unit_test and fix the error

--- Searching/Interpolation Search.py
+++ Searching/Interpolation Search.py
@@ -17,14 +17,25 @@ It estimates the position of the target value by using a formula based on the ar
 def interpolation_search(arr, target):
     left = 0
     right = len(arr) - 1
-    while left < right:
+
+    while left <= right and arr[left] <= target <= arr[right]:
+        if arr[left] == target:
+            return left
+
+        if right < left:
+            break
+
         mid = (target - arr[left]) * (right - left) // (arr[right] - arr[left]) + left
+
         if arr[mid] == target:
             return mid
+
         if arr[mid] < target:
             left = mid + 1
+
         else:
             right = mid - 1
+
     return -1
 
 
@@ -33,12 +44,35 @@ def interpolation_search(arr, target):
 # Worst-Case Time complexity = O(n)
 # Space complexity = O(1)
 
-arr = [1, 4, 6, 9, 12, 15, 16, 25]
-target = 16
 
-result = interpolation_search(arr, target)
+import unittest
 
-if result == -1:
-    print("Element not found")
-else:
-    print(f"Element found at index {result}")
+
+class TestInterpolationSearch(unittest.TestCase):
+    def test_interpolation_search_found(self):
+        arr = [1, 2, 3, 4, 5]
+        target = 3
+        result = interpolation_search(arr, target)
+        self.assertEqual(result, 2)
+
+    def test_interpolation_search_not_found(self):
+        arr = [1, 2, 3, 4, 5]
+        target = 6
+        result = interpolation_search(arr, target)
+        self.assertEqual(result, -1)
+
+    def test_interpolation_search_empty_array(self):
+        arr = []
+        target = 6
+        result = interpolation_search(arr, target)
+        self.assertEqual(result, -1)
+
+    def test_interpolation_search_single_element(self):
+        arr = [3]  # single element array
+        target = 3  # element in array
+        result = interpolation_search(arr, target)  # search for element
+        self.assertEqual(result, 0)  # should return index 0
+
+
+if __name__ == "__main__":
+    unittest.main()
